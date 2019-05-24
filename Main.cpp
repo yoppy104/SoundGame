868edfe04606fd1c71df60b4fd3d6bbc909d1b93
Yoppy <beretta92f.104@gmail.com>
2019-05-24 18:42:13 +0900

    "Notesクラスの宣言を作成"

--- Main.cpp
+++ Main.cpp
@@ -3,13 +3,15 @@
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	ChangeWindowMode(true);
+
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
-	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
-
+	while (CheckHitKey(KEY_INPUT_ESCAPE) == 0 && ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+		DrawFormatString(10, 10, GetColor(255, 0, 0), "test");
 	}
 
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
--- Notes.h
+++ Notes.h
@@ -1,8 +1,28 @@
 #pragma once
 
+enum Type {
+	SINGLE,
+	HOLD
+};
+
 class Notes {
 private:
+	Type type_m;
+
+	//長押しの判定に使う数値。SINGLEの場合は1で初期化
+	int holdTime_m;
+
+	//開始フレームからノーツが画面上に出現するまでの基準フレーム数
+	//実際にはBPMによる重みづけをする
+	int appearFrame_m;
 
 public:
+	//SINGLE用
 	Notes();
+	//HOLD用
+	Notes(int hold_time);
+
+	Type getType()        const { return type_m; }
+	int  getHoldTime()    const { return holdTime_m; }
+	int  getAppearFrame() const { return appearFrame_m; }
 };
